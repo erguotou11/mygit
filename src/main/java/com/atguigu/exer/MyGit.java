dcb011587b6e178e4bc2bdbaca2e5b1f29cdba55
fjc <1786079681@qq.com>
2019-06-24 23:57:26 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/exer/MyGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.exer;
+
+/**
+ * @author FJC
+ * @create 2019-06-24-23:44
+ */
+public class MyGit {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
